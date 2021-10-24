97720899145a691baf870c37884aecd028905c6c
prosper007 <onungwap@gmail.com>
2021-10-24 08:26:58 -0400

    start afresh with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Grid, Paper } from "@mui/material";
+import { useSpring, animated, config } from "react-spring";
 
-function App() {
+const App = () => {
+  const classes = {
+    containerRoot: {
+      height: "100vh",
+      width: "100vw",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "gray",
+    },
+    paperRoot: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "16em",
+      width: "14em",
+      borderRadius: "0.75em",
+      backgroundColor: "red",
+    }
+  }
+  const AnimatedPaper = animated(Paper);
+  const springProps = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    delay: 200,
+    loop: { reverse: true },
+  });
+  const [flip, set] = useState(false)
+  const { number } = useSpring({
+    reset: true,
+    reverse: flip,
+    from: { number: 0 },
+    number: 1,
+    config: config.molasses,
+    onRest: () => set(!flip),
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container sx={classes.containerRoot}>
+      {/* <animated.div style={springProps}> */}
+      <AnimatedPaper
+        elevation={3}
+        sx={classes.paperRoot}
+        style={springProps}
+      >
+        {number.to(n => n.toFixed(2))}
+      </AnimatedPaper>
+      {/* </animated.div> */}
+    </Grid>
   );
 }
 
