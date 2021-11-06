54027222651de7125316f43f37976d3b6b17e5aa
prosper007 <onungwap@gmail.com>
2021-11-06 15:28:50 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,13 @@
-import { useState } from "react";
-import { Grid, Paper } from "@mui/material";
-import { useSpring, animated, config } from "react-spring";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Take1 from "./Takes/Take1";
 
 const App = () => {
-  const classes = {
-    containerRoot: {
-      height: "100vh",
-      width: "100vw",
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "gray",
-    },
-    paperRoot: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "16em",
-      width: "14em",
-      borderRadius: "0.75em",
-      backgroundColor: "red",
-    }
-  }
-  const AnimatedPaper = animated(Paper);
-  const springProps = useSpring({
-    to: { opacity: 1 },
-    from: { opacity: 0 },
-    delay: 200,
-    loop: { reverse: true },
-  });
-  const [flip, set] = useState(false)
-  const { number } = useSpring({
-    reset: true,
-    reverse: flip,
-    from: { number: 0 },
-    number: 1,
-    config: config.molasses,
-    onRest: () => set(!flip),
-  })
   return (
-    <Grid container sx={classes.containerRoot}>
-      {/* <animated.div style={springProps}> */}
-      <AnimatedPaper
-        elevation={3}
-        sx={classes.paperRoot}
-        style={springProps}
-      >
-        {number.to(n => n.toFixed(2))}
-      </AnimatedPaper>
-      {/* </animated.div> */}
-    </Grid>
-  );
+    <Routes>
+      <Route path="/" element={<Navigate to="/take1" />} />
+      <Route path="/take1" element={<Take1 />} />
+    </Routes>
+  )
 }
 
 export default App;
