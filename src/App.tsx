394ae14a53fc3f106ee98b3c29f185ee8a36d73b
prosper007 <onungwap@gmail.com>
2021-11-07 12:29:25 -0500

    set up example; take 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Take1 from "./Takes/Take1";
+import Take2 from "./Takes/Take2";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/take1" />} />
       <Route path="/take1" element={<Take1 />} />
+      <Route path="/take2" element={<Take2 />} />
     </Routes>
   )
 }
---
+++ src/Takes/Take2.tsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import { useSpring, animated, config } from "@react-spring/web";
+import { useControls } from "leva";
+
+import "./Take2styles.css";
+
+const calc = (x: number, y: number, rect: DOMRect) => [
+  -(y - rect.top - rect.height / 2) / 5,
+  (x - rect.left - rect.width / 2) / 5,
+  1.4
+];
+const trans = (x: number, y: number, s: number) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+type ConfigOptions = keyof typeof config;
+type ControlOptions = "preset";
+interface ControlConfig {
+  value: ConfigOptions;
+  options: ConfigOptions[];
+}
+
+export default function Take2() {
+  const configList = Object.keys(config) as ConfigOptions[];
+  const ref = useRef<HTMLDivElement>(null);
+  const [xys, set] = useState([0, 0, 1]);
+  const { preset } = useControls<Record<ControlOptions, ControlConfig>, Record<ControlOptions, ControlConfig>, Record<ControlOptions, ControlConfig>>({
+    preset: { value: "default", options: configList }
+  });
+  const props = useSpring({ xys, config: config[preset] });
+  return (
+    <div className="ccard-main" ref={ref}>
+      <animated.div
+        className="ccard"
+        style={{ transform: props.xys.to(trans) }}
+        onMouseLeave={() => set([0, 0, 1])}
+        onMouseMove={(e) => {
+          if (ref.current) {
+            const rect = ref.current.getBoundingClientRect();
+            set(calc(e.clientX, e.clientY, rect));
+          }
+        }}
+      />
+    </div>
+  );
+}
