33b7fc26d69f83d6125d0680a6443d5f5b00b82d
prosper007 <onungwap@gmail.com>
2023-01-03 20:19:29 -0500

    start/stop oscillation onClick

--- src/Takes/Take3.tsx
+++ src/Takes/Take3.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSpring, animated, config, easings } from "@react-spring/web";
 import { useControls } from "leva";
 import { Box } from "@mui/material";
@@ -16,24 +16,28 @@ export default function Take3() {
   const { preset } = useControls<Record<ControlOptions, ControlConfig>, Record<ControlOptions, ControlConfig>, Record<ControlOptions, ControlConfig>>({
     preset: { value: "default", options: configList }
   });
-  const [flip, setFlip] = useState(false);
+  const [animate, setAnimate] = useState(false);
   const [styles, api] = useSpring(() => ({
-    // reverse: flip,
     from: { x: 0 },
-    // to: { x: 60 },
-    // onRest: () => setFlip(!flip),
-    // config: config[preset]
     // config: { tension: 10, friction: 10, mass: 1,  }
     // config: { duration: 500, easing: easings.easeInOutElastic }
   }))
 
   const handleClick = () => {
-    api.start({
-      from: { x: 0 },
-      to: [{ x: 60 }, { x: -60 }],
-      loop: { from: { x: -60 } },
-    })
+    setAnimate(!animate)
   }
+
+  useEffect(() => {
+    if (animate) {
+      api.start({
+        to: [{ x: 80 }, { x: -80 }],
+        loop: { from: { x: -80 } },
+        config: config[preset]
+      })
+    } else {
+      api.stop()
+    }
+  }, [animate, api, preset])
 
 
   return (
@@ -55,9 +59,9 @@ export default function Take3() {
           borderRadius: "5px",
           transition: "box-shadow 0.5s",
           willChange: "transform",
+          cursor: "pointer"
         }}
         onClick={handleClick}
-      // style={{ transform: xPosSpring.to(trans) }}
       />
     </Box>
   );
